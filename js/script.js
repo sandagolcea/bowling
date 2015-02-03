44cf4585a5e8ab9bf3892e17bd1709cbbfb2a2ad
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-03 02:08:21 +0000

    Can modify 1rst frame value

--- js/script.js
+++ js/script.js
@@ -2,5 +2,16 @@ $(document).ready(function(){
   
   game = new Game();
   $('table').after("<p>You have "+game.rollsLeft+" rolls left</p>");
-  alert("Ready to play!");
+  // alert("Ready to play!");
+
+  $('.scores').val(" ");
+
+  // Addition input box and button here:
+  $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();   
+        // To simply output the value in the input box:
+        // $('.list').append("<div class='item'>" + toAdd + "</div>");
+        $('#score1').val(toAdd);
+  });
+
 });
